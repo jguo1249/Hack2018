2ebf17ebd801c15e0bd099be03b57d43c258f712
jason1249 <jxgformal@gmail.com>
2018-10-27 13:15:18 -0400

    news

---
+++ serverMongo/flaskr/db.py
@@ -0,0 +1,43 @@
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+from flask_pymongo import PyMongo
+
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
+def get_db():
+    if 'db' not in g:
+        app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+        g.db = PyMongo(app).db
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
