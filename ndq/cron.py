ebf428a6d82c74aa7903fb1c2d6513eeeabc08b2
Samuel Stevens <samuel.robert.stevens@gmail.com>
2018-10-28 09:21:23 -0400

    working on cron

--- ndq/cron.py
+++ ndq/cron.py
@@ -1,8 +1,15 @@
 import time
 
+import requests
 import schedule
 
-from ndq.news import update_articles
+
+def update_articles():
+    r = requests.post("http://localhost:8000/update-articles")
+    print(r.status_code, r.reason)
+
+
+update_articles()
 
 schedule.every(1).hour.do(update_articles)
 
